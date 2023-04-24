23521bc2a067ad59857743d3ab4f12766ca4d506
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2023-04-23 20:26:13 -0500

    Successfully displayed database  into HTML

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
-const express = require ('express'); 
-const mongoose = require('mongoose'); 
-const app = express(); 
-const ejs = require('ejs'); 
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const ejs = require("ejs");
+const { boolean, double } = require("webidl-conversions");
+const { Double, Decimal128 } = require("bson");
 
-app.set('view engine', 'ejs'); 
+app.set("view engine", "ejs");
 
- mongoose.connect('mongodb+srv://admin-brandon:test123@cluster0.7drueob.mongodb.net/Docket'); 
+main().catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://admin-brandon:test123@cluster0.7drueob.mongodb.net/Docket"
+  );
 
-})
+  const docketSchema = {
+    firstName: String,
+    lastName: String,
+    DOB: Date,
+    UScitizen: Boolean,
+    Height: String,
+    Weight: String,
+    Case: String,
+    Court: String,
+    JudgeName: String,
+    CourtType: String,
+    CourtFloor: String,
+    RaceSex: String,
+    NextCourtDate: Date,
+    Status: String,
+  };
 
- app.listen(4000, function(){
-
-  console.log('server is running'); 
- })
-
+  const Defendant = mongoose.model("Defendant", docketSchema);
+  const defendants = await Defendant.find();
+  //console.log(defendants);
+  app.get("/", (req, res) => {
+    res.render("index", {
+      defList: defendants,
+    });
+  });
+  app.listen(4000, function () {
+    console.log("server is running");
+  });
+}
